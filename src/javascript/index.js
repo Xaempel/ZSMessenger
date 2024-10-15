3208ea85909ea4dd9bb7683b545a1097f8e9c786
Xaempel <xaempelfeedback@gmail.com>
2024-10-15 18:18:27 +0200

    <Feature> -- Developed a new sketch of site style

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,8 @@
-console.log("I'm Test JS Message")
+function clearInputBox() {
+  let InputBoxName = document.getElementsByClassName('InputBox').item(0);
+  let InputBoxSurName = document.getElementsByClassName('InputBox').item(1);
+  let InputBoxPassword = document.getElementsByClassName('InputBox').item(2);
+  InputBoxName.value = '';
+  InputBoxSurName.value = '';
+  InputBoxPassword.value = '';
+}
